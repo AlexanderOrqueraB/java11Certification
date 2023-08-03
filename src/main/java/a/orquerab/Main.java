0649a08ebea5207dd5b10e0071e54dd74d943055
alexander-orquera <alexander.orquera@pagonxt.com>
2023-08-03 13:08:35 +0200

    First change in Main.java

--- src/main/java/a/orquerab/Main.java
+++ src/main/java/a/orquerab/Main.java
@@ -2,6 +2,6 @@ package a.orquerab;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java 11.");
+        System.out.println("Probando primer commit");
     }
 }
